0aa6147ff9bf49e38af19d44d64fa9ebf141037a
Andrey Naumov <andreu.naumoff@gmail.com>
2022-12-17 00:16:10 +0300

    Changed file extension js -> ts

--- src/shared/utils/params-to-query-string.ts
+++ src/shared/utils/params-to-query-string.ts
@@ -1,4 +1,4 @@
-export const paramsToQueryString = queryParams => {
+export const paramsToQueryString = <T extends Record<string, string>>(queryParams: T) => {
   return Object.entries(queryParams).reduce((queryStringAcc, [paramKey, paramValue]) => {
     const prefix = queryStringAcc.length ? '&' : '?';
     const currentParam = `${prefix}${paramKey}=${paramValue}`;
