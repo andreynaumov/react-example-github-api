d8050a900cbcbb6866e69dd730ef802909c7d60f
Andrey Naumov <andreu.naumoff@gmail.com>
2022-12-16 23:47:49 +0300

    Updated .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,12 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -12,13 +17,11 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     'no-var': 'error',
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
-
-    // allow jsx syntax in js files (for next.js project)
-    // 'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}], //should add ".ts" if typescript project
+    '@typescript-eslint/no-explicit-any': 'error',
   },
 };
